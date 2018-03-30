777e6ba7eeb72281fe33d392aecd91b02eb144be
Donovan Bergin <kaiserbergin@gmail.com>
2018-03-30 02:58:58 -0500

    initial commit

---
+++ Assets/PerformanceBounceback/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public GameManager gameManager;
+    public string scoreLabel = "Score: ";
+
+    private int displayedScore = -1;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(displayedScore != gameManager.score) {
+            Text text = GetComponentInChildren<Text>();
+            text.text = "Score: " + gameManager.score.ToString();
+        }		
+	}
+}
